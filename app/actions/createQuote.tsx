31fe7967c08f93a4f237be8e64b018307d1f2c62
rowgregory <rowgregory@gmail.com>
2024-06-24 12:05:16 -0400

    chore(vercel):Add Vercel deployment URL to createQuote function

--- app/actions/createQuote.tsx
+++ app/actions/createQuote.tsx
@@ -4,7 +4,7 @@ const quoteReceivedURL2 = "/audio/quote-received-2.mp3";
 
 async function createQuote(quote: any) {
   const received = new UIfx(quoteReceivedURL2);
-  const response = await fetch("/api/quote?endpoint=REQUEST_A_QUOTE", {
+  const response = await fetch("https://sqysh.vercel.app/api/quote?endpoint=REQUEST_A_QUOTE", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
