0b0c7b8d2a12dbc90673f1f2ae39c4f2eff0bb20
Gregory Row <sqysh@Gregorys-Mac-mini.local>
2025-05-05 09:55:41 -0400

    chore(api):Add www to live url

--- app/actions/createQuote.tsx
+++ app/actions/createQuote.tsx
@@ -4,7 +4,7 @@ const quoteReceivedURL2 = "/audio/quote-received-2.mp3";
 async function createQuote(quote: any) {
   const received = new UIfx(quoteReceivedURL2);
   const response = await fetch(
-    "https://sqysh.io/api/quote?endpoint=REQUEST_A_QUOTE",
+    "https://www.sqysh.io/api/quote?endpoint=REQUEST_A_QUOTE",
     {
       method: "POST",
       headers: {
