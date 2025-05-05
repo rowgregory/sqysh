3dc83d16ee259e8f8bf760e0b0e6011dfb64afdd
Gregory Row <sqysh@Gregorys-Mac-mini.local>
2025-05-05 08:59:24 -0400

    chore(api):Update custom domain name in api routes

--- app/actions/createQuote.tsx
+++ app/actions/createQuote.tsx
@@ -1,16 +1,18 @@
-
 import UIfx from "uifx";
 const quoteReceivedURL2 = "/audio/quote-received-2.mp3";
 
 async function createQuote(quote: any) {
   const received = new UIfx(quoteReceivedURL2);
-  const response = await fetch("https://sqysh.vercel.app/api/quote?endpoint=REQUEST_A_QUOTE", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ quote }),
-  });
+  const response = await fetch(
+    "https://sqysh.io/api/quote?endpoint=REQUEST_A_QUOTE",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ quote }),
+    }
+  );
   if (response.ok) {
     received.play();
     return await response.json();
